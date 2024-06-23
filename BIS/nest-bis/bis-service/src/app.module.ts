e2d6c8ff959578c5faaf08861ee8da1848648272
DanielHolloway <daniel@vendettistudios.com>
2024-06-22 18:54:51 -0700

    edits to app.module and main ts

--- BIS/nest-bis/bis-service/src/app.module.ts
+++ BIS/nest-bis/bis-service/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MetricsController } from './metrics.controller'; // Add this import
+import { MetricsService } from './metrics.service'; // Add this import
 
 @Module({
   imports: [],
